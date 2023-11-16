b1901473ec6e4b6e0f449f859f04cc03ee4f9723
Miguel Uve <miguel.vilata@gmail.com>
2023-11-16 17:57:05 +0100

    8.4 Generating a Controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,6 @@
 class Article < ApplicationRecord
-    class Article < ApplicationRecord
-        has_many :comments
-        
-        validates :title, presence: true
-        validates :body, presence: true, length: { minimum: 10 }
-      end    
-end
+    has_many :comments
+    
+    validates :title, presence: true
+    validates :body, presence: true, length: { minimum: 10 }
+end    
