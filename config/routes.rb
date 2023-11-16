1631c9e3a39b8392152882b5d211c1633a25ee7e
Miguel Uve <miguel.vilata@gmail.com>
2023-11-16 17:35:09 +0100

    8.3 Adding a Route for Comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "articles#index"
 
   resources :articles
+    resources :comments
 end
 
 Rails.application.routes.draw do
@@ -20,4 +21,3 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-
